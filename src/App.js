207b91217e7a1733d43d28b0aa400eff0d15851a
ahmadammmoura <ammoura575@gmail.com>
2021-09-02 14:49:47 +0300

    done

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function App() {
   return (
     <>
       {/* <Home /> */}
-      <AllBarbers />
+      {/* <AllBarbers /> */}
       {/* <ClientProfile /> */}
-      {/* <BarberProfile /> */}
+      <BarberProfile />
       {/* <Products />
       <SignInUp /> */}
     </>
