42fc0c425ae3706b9c0701e8c29fd25fe4e6b2cb
hatem <hatemhtm7@gmail.com>
2021-09-02 05:20:16 +0300

    barber profile

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 // import Home from './components/Home';
 // import AllBarbers from './components/AllBarbers';
 // import ClientProfile from './components/ClientProfile';
-// import BarberProfile from './components/BarberProfile';
 // import Products from './components/Products';
+// import BarberProfile from "./components/barber/BarberProfile";
 // import SignInUp from './components/SignInUp';
 function App() {
   return (
     <>
       {/* <Home />
       <AllBarbers />
-      <ClientProfile />
-      <BarberProfile />
-      <Products />
+      <ClientProfile /> */}
+      {/* <BarberProfile /> */}
+      {/* <Products />
       <SignInUp /> */}
     </>
   );
