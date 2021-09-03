560f84a9a6998d137b46ad0ce67167e4e8367523
ahmadammmoura <ammoura575@gmail.com>
2021-09-03 11:25:58 +0300

    use module css

--- src/components/home/TobRated.jsx
+++ src/components/home/TobRated.jsx
@@ -1,98 +1,101 @@
 import React from "react";
-import "./style/recom.css";
 import { Link } from "react-router-dom";
 
+import styles from './style/recom.module.css'
+
 function TobRated() {
+
   return (
-    <div className="rateContainer">
-
-        
-      <div className="recomendationBox">
-      <h2 className='topRatedH' >Top Rated</h2>
-        <figure class="snip1515">
-          <div class="profile-image">
-            <Link to='/barber-Profile/5'><img
-              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample47.jpg"
-              alt="sample47"
-            />
-            </Link>
-          </div>
-          <figcaption>
-            <h3>Fleece Marigold</h3>
-            <h4>Founder</h4>
-            <p>
-              Which is worse, that everyone has his price, or that the price is
-              always so low.
-            </p>
-          </figcaption>
-        </figure>
-        <figure class="snip1515">
-          <div class="profile-image">
-            <img
-              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample47.jpg"
-              alt="sample47"
-            />
-          </div>
-          <figcaption>
-            <h3>Fleece Marigold</h3>
-            <h4>Founder</h4>
-            <p>
-              Which is worse, that everyone has his price, or that the price is
-              always so low.
-            </p>
-          </figcaption>
-        </figure>
-        <figure class="snip1515">
-          <div class="profile-image">
-            <img
-              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample47.jpg"
-              alt="sample47"
-            />
-          </div>
-          <figcaption>
-            <h3>Fleece Marigold</h3>
-            <h4>Founder</h4>
-            <p>
-              Which is worse, that everyone has his price, or that the price is
-              always so low.
-            </p>
-          </figcaption>
-        </figure>
-
-        <figure class="snip1515">
-          <div class="profile-image">
-            <img
-              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample47.jpg"
-              alt="sample47"
-            />
-          </div>
-          <figcaption>
-            <h3>Fleece Marigold</h3>
-            <h4>Founder</h4>
-            <p>
-              Which is worse, that everyone has his price, or that the price is
-              always so low.
-            </p>
-          </figcaption>
-        </figure>
-
-        <figure class="snip1515">
-          <div class="profile-image">
-            <img
-              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample47.jpg"
-              alt="sample47"
-            />
-          </div>
-          <figcaption>
-            <h3>Fleece Marigold</h3>
-            <h4>Founder</h4>
-            <p>
-              Which is worse, that everyone has his price, or that the price is
-              always so low.
-            </p>
-          </figcaption>
-        </figure>
+    <div className={styles.rateContainer}>
+        <div className={styles.wrapper}>
+    <div className={styles.imgArea}>
+      <div className={styles.innerArea}>
+        <img src="https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt=""/>
       </div>
+    </div>
+    <div className={`${styles.icon} ${styles.arrow}`}><i className="fas fa-arrow-left"></i></div>
+    <div className={`${styles.icon} ${styles.dots}`}><i className="fas fa-ellipsis-v"></i></div>
+    <div className={styles.name}>CodingNepal</div>
+    <div className={styles.about}>Designer & Developer</div>
+    <div className={styles.socialIcons}>
+      <a href="#" className={styles.fb}><i className="fab fa-facebook-f"></i></a>
+      <a href="#" className={styles.twitter}><i className="fab fa-twitter"></i></a>
+      <a href="#" className={styles.insta}><i className="fab fa-instagram"></i></a>
+      <a href="#" className={styles.yt}><i className="fab fa-youtube"></i></a>
+    </div>
+    <div className={styles.buttons}>
+      <button>Message</button>
+      <button>Subscribe</button>
+    </div>
+  </div>
+
+  <div className={styles.wrapper}>
+    <div className={styles.imgArea}>
+      <div className={styles.innerArea}>
+        <img src="https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt=""/>
+      </div>
+    </div>
+    <div className={`${styles.icon} ${styles.arrow}`}><i className="fas fa-arrow-left"></i></div>
+    <div className={`${styles.icon} ${styles.dots}`}><i className="fas fa-ellipsis-v"></i></div>
+    <div className={styles.name}>CodingNepal</div>
+    <div className={styles.about}>Designer & Developer</div>
+    <div className={styles.socialIcons}>
+      <a href="#" className={styles.fb}><i className="fab fa-facebook-f"></i></a>
+      <a href="#" className={styles.twitter}><i className="fab fa-twitter"></i></a>
+      <a href="#" className={styles.insta}><i className="fab fa-instagram"></i></a>
+      <a href="#" className={styles.yt}><i className="fab fa-youtube"></i></a>
+    </div>
+    <div className={styles.buttons}>
+      <button>Message</button>
+      <button>Subscribe</button>
+    </div>
+  </div>
+
+  <div className={styles.wrapper}>
+    <div className={styles.imgArea}>
+      <div className={styles.innerArea}>
+        <img src="https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt=""/>
+      </div>
+    </div>
+    <div className={`${styles.icon} ${styles.arrow}`}><i className="fas fa-arrow-left"></i></div>
+    <div className={`${styles.icon} ${styles.dots}`}><i className="fas fa-ellipsis-v"></i></div>
+    <div className={styles.name}>CodingNepal</div>
+    <div className={styles.about}>Designer & Developer</div>
+    <div className={styles.socialIcons}>
+      <a href="#" className={styles.fb}><i className="fab fa-facebook-f"></i></a>
+      <a href="#" className={styles.twitter}><i className="fab fa-twitter"></i></a>
+      <a href="#" className={styles.insta}><i className="fab fa-instagram"></i></a>
+      <a href="#" className={styles.yt}><i className="fab fa-youtube"></i></a>
+    </div>
+    <div className={styles.buttons}>
+      <button>Message</button>
+      <button>Subscribe</button>
+    </div>
+  </div>
+
+  <div className={styles.wrapper}>
+    <div className={styles.imgArea}>
+      <div className={styles.innerArea}>
+        <img src="https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt=""/>
+      </div>
+    </div>
+    <div className={`${styles.icon} ${styles.arrow}`}><i className="fas fa-arrow-left"></i></div>
+    <div className={`${styles.icon} ${styles.dots}`}><i className="fas fa-ellipsis-v"></i></div>
+    <div className={styles.name}>CodingNepal</div>
+    <div className={styles.about}>Designer & Developer</div>
+    <div className={styles.socialIcons}>
+      <a href="#" className={styles.fb}><i className="fab fa-facebook-f"></i></a>
+      <a href="#" className={styles.twitter}><i className="fab fa-twitter"></i></a>
+      <a href="#" className={styles.insta}><i className="fab fa-instagram"></i></a>
+      <a href="#" className={styles.yt}><i className="fab fa-youtube"></i></a>
+    </div>
+    <div className={styles.buttons}>
+      <button>Message</button>
+      <button>Subscribe</button>
+    </div>
+  </div>
+  
+        
     </div>
   );
 }
