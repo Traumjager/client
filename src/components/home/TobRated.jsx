ffc7ce4397857e5c6cc0422cd342bc82e96694d4
ahmadammmoura <ammoura575@gmail.com>
2021-09-03 15:43:58 +0300

    home 90%  done

--- src/components/home/TobRated.jsx
+++ src/components/home/TobRated.jsx
@@ -1,102 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
-import styles from './style/recom.module.css'
-
+import styles from "./style/recom.module.css";
+import RatedCard from "./RatedCard";
 function TobRated() {
-
-  return (
+  return (<>
+  <h2 className={styles.topRatedH} >Top Rated</h2>
     <div className={styles.rateContainer}>
-        <div className={styles.wrapper}>
-    <div className={styles.imgArea}>
-      <div className={styles.innerArea}>
-        <img src="https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt=""/>
-      </div>
+      <RatedCard/>
+      <RatedCard/>
+      <RatedCard/>
+      <RatedCard/>
     </div>
-    <div className={`${styles.icon} ${styles.arrow}`}><i className="fas fa-arrow-left"></i></div>
-    <div className={`${styles.icon} ${styles.dots}`}><i className="fas fa-ellipsis-v"></i></div>
-    <div className={styles.name}>CodingNepal</div>
-    <div className={styles.about}>Designer & Developer</div>
-    <div className={styles.socialIcons}>
-      <a href="#" className={styles.fb}><i className="fab fa-facebook-f"></i></a>
-      <a href="#" className={styles.twitter}><i className="fab fa-twitter"></i></a>
-      <a href="#" className={styles.insta}><i className="fab fa-instagram"></i></a>
-      <a href="#" className={styles.yt}><i className="fab fa-youtube"></i></a>
-    </div>
-    <div className={styles.buttons}>
-      <button>Message</button>
-      <button>Subscribe</button>
-    </div>
-  </div>
-
-  <div className={styles.wrapper}>
-    <div className={styles.imgArea}>
-      <div className={styles.innerArea}>
-        <img src="https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt=""/>
-      </div>
-    </div>
-    <div className={`${styles.icon} ${styles.arrow}`}><i className="fas fa-arrow-left"></i></div>
-    <div className={`${styles.icon} ${styles.dots}`}><i className="fas fa-ellipsis-v"></i></div>
-    <div className={styles.name}>CodingNepal</div>
-    <div className={styles.about}>Designer & Developer</div>
-    <div className={styles.socialIcons}>
-      <a href="#" className={styles.fb}><i className="fab fa-facebook-f"></i></a>
-      <a href="#" className={styles.twitter}><i className="fab fa-twitter"></i></a>
-      <a href="#" className={styles.insta}><i className="fab fa-instagram"></i></a>
-      <a href="#" className={styles.yt}><i className="fab fa-youtube"></i></a>
-    </div>
-    <div className={styles.buttons}>
-      <button>Message</button>
-      <button>Subscribe</button>
-    </div>
-  </div>
-
-  <div className={styles.wrapper}>
-    <div className={styles.imgArea}>
-      <div className={styles.innerArea}>
-        <img src="https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt=""/>
-      </div>
-    </div>
-    <div className={`${styles.icon} ${styles.arrow}`}><i className="fas fa-arrow-left"></i></div>
-    <div className={`${styles.icon} ${styles.dots}`}><i className="fas fa-ellipsis-v"></i></div>
-    <div className={styles.name}>CodingNepal</div>
-    <div className={styles.about}>Designer & Developer</div>
-    <div className={styles.socialIcons}>
-      <a href="#" className={styles.fb}><i className="fab fa-facebook-f"></i></a>
-      <a href="#" className={styles.twitter}><i className="fab fa-twitter"></i></a>
-      <a href="#" className={styles.insta}><i className="fab fa-instagram"></i></a>
-      <a href="#" className={styles.yt}><i className="fab fa-youtube"></i></a>
-    </div>
-    <div className={styles.buttons}>
-      <button>Message</button>
-      <button>Subscribe</button>
-    </div>
-  </div>
-
-  <div className={styles.wrapper}>
-    <div className={styles.imgArea}>
-      <div className={styles.innerArea}>
-        <img src="https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt=""/>
-      </div>
-    </div>
-    <div className={`${styles.icon} ${styles.arrow}`}><i className="fas fa-arrow-left"></i></div>
-    <div className={`${styles.icon} ${styles.dots}`}><i className="fas fa-ellipsis-v"></i></div>
-    <div className={styles.name}>CodingNepal</div>
-    <div className={styles.about}>Designer & Developer</div>
-    <div className={styles.socialIcons}>
-      <a href="#" className={styles.fb}><i className="fab fa-facebook-f"></i></a>
-      <a href="#" className={styles.twitter}><i className="fab fa-twitter"></i></a>
-      <a href="#" className={styles.insta}><i className="fab fa-instagram"></i></a>
-      <a href="#" className={styles.yt}><i className="fab fa-youtube"></i></a>
-    </div>
-    <div className={styles.buttons}>
-      <button>Message</button>
-      <button>Subscribe</button>
-    </div>
-  </div>
-  
-        
-    </div>
+    </>
   );
 }
 
