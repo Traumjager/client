3e4c177217ebba4466ba31aafdc0713b901763d8
ahmadammmoura <ammoura575@gmail.com>
2021-09-05 13:34:54 +0300

    done

--- src/components/allBarbers/index.js
+++ src/components/allBarbers/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import AllBarbers from './components/AllBarbers'
+import SearchBar from './components/SearchBar'
 
+import styles from './style/main.module.css'
 function index() {
     return (
-        <div>
-            
+        <div className={styles.main}>
+            <SearchBar/>
+            <AllBarbers/>
         </div>
     )
 }
